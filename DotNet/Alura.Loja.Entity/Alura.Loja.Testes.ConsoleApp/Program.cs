e2c0895464e6de3256826daea6e1c20166596246
Jefferson Silverio <jsilverio@ciandt.com>
2021-03-17 18:59:39 -0300

    Commit inicial do curso de entity framework

---
+++ DotNet/Alura.Loja.Entity/Alura.Loja.Testes.ConsoleApp/Program.cs
@@ -0,0 +1,42 @@
+﻿using Alura.Loja.Testes.ConsoleApp.Context;
+
+namespace Alura.Loja.Testes.ConsoleApp
+{
+    public static class Program
+    {
+        private static void Main(string[] args)
+        {
+            GravarUsandoEntity();
+        }
+
+        private static void GravarUsandoEntity()
+        {
+            var p1 = new Produto
+            {
+                Nome = "Harry Potter e a Ordem da Fênix",
+                Categoria = "Livros",
+                Preco = 19.89
+            };
+
+            var p2 = new Produto
+            {
+                Nome = "Senhor dos Anéis 1",
+                Categoria = "Livros",
+                Preco = 19.89
+            };
+
+            var p3 = new Produto
+            {
+                Nome = "O Monge e o Executivo",
+                Categoria = "Livros",
+                Preco = 19.89
+            };
+
+            using (var contexto = new LojaContext())
+            {
+                contexto.Produtos.AddRange(p1, p2, p3);
+                contexto.SaveChanges();
+            }
+        }
+    }
+}
