0b5fdf9da4c8451472140ca4e127b2782d961b8c
Jefferson Silverio <jsilverio@ciandt.com>
2021-03-20 15:34:14 -0300

    Convenção de nomes de tabela e coluna

--- DotNet/Alura.Filmes.Entity/Alura.Filmes.App/Program.cs
+++ DotNet/Alura.Filmes.Entity/Alura.Filmes.App/Program.cs
@@ -1,9 +1,22 @@
-﻿namespace Alura.Filmes.App
+﻿using Alura.Filmes.App.Dados;
+using Alura.Filmes.App.Extensions;
+using System;
+
+namespace Alura.Filmes.App
 {
     public static class Program
     {
         public static void Main()
         {
+            using (var contexto = new AluraFilmesContexto())
+            {
+                contexto.LogSQLToConsole();
+
+                foreach (var ator in contexto.Atores)
+                {
+                    Console.WriteLine(ator);
+                }
+            }
         }
     }
 }
