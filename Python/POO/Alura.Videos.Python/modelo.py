6cd5675c7563dac2f22aab94fc19eb46f8dc5c10
Jefferson Silverio <jsilverio@ciandt.com>
2021-04-27 18:48:53 -0300

    Finalizando curso Python 3: Avançando na orientação a objetos

---
+++ Python/POO/Alura.Videos.Python/modelo.py
@@ -0,0 +1,88 @@
+from collections.abc import MutableSequence
+
+class Programa:
+    def __init__(self, nome, ano):
+        self._nome = nome.title()
+        self.ano = ano
+        self._likes = 0
+
+    @property
+    def likes(self):
+        return self._likes
+
+    def dar_likes(self):
+        self._likes += 1
+
+    @property
+    def nome(self):
+        return self._nome
+
+    @nome.setter
+    def nome(self, novo_nome):
+        self._nome = novo_nome.title()
+
+    def __str__(self):
+        return print(f'Nome: {programa.nome} - Likes: {programa.likes}')
+
+
+class Filme(Programa):
+    def __init__(self, nome, ano, duracao):
+        super().__init__(nome, ano)
+        self.duracao = duracao
+
+    def __str__(self):
+        return f'Nome: {programa.nome} - {self.duracao} Minutos - Likes: {programa.likes}'
+
+
+class Serie(Programa):
+    def __init__(self, nome, ano, temporadas):
+        super().__init__(nome, ano)
+        self.temporadas = temporadas
+
+    def __str__(self):
+        return f'Nome: {programa.nome} - {self.temporadas} Temporadas - Likes: {programa.likes}'
+
+
+class Playlist(MutableSequence):
+    def __init__(self, nome, programas):
+        self.nome = nome
+        self._programas = programas
+
+    def __getitem__(self, item):
+        return self._programas[item]
+
+    @property
+    def listagem(self):
+        return self._programas
+
+    def __len__(self):
+        return len(self._programas)
+
+
+vingadores = Filme('vingadores - guerra infinita', 2018, 160)
+atlanta = Serie('atlanta', 2018, 2)
+tmep = Filme('Todo mundo em pânico', 1999, 100)
+demolidor = Serie('Demolidor', 2016, 2)
+
+vingadores.dar_likes()
+tmep.dar_likes()
+tmep.dar_likes()
+tmep.dar_likes()
+tmep.dar_likes()
+demolidor.dar_likes()
+demolidor.dar_likes()
+atlanta.dar_likes()
+atlanta.dar_likes()
+atlanta.dar_likes()
+
+filmes_e_series = [vingadores, atlanta,  tmep]
+playlist_fim_de_semana = Playlist('fim de semana', filmes_e_series)
+
+print(f'Tamanho do playlist: {len(playlist_fim_de_semana)}')
+
+
+print(f'Tá ou não tá? {demolidor in playlist_fim_de_semana}')
+
+for programa in playlist_fim_de_semana:
+    print(programa)
+
