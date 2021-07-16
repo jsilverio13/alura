ba66159aa3de590ab4ad1f2ed67905071e029167
Jefferson Silverio <jsilverio@ciandt.com>
2021-07-15 22:29:07 -0300

    Finalizando curso API Rest com .NET Core 5: Operações essenciais com verbos HTTP

---
+++ DotNet/WebAPI/Alura.Filmes.Api/Alura.Filmes.Api/Profiles/FilmeProfile.cs
@@ -0,0 +1,16 @@
+﻿using Alura.Filmes.Api.Data.Dto;
+using Alura.Filmes.Api.Models;
+using AutoMapper;
+
+namespace Alura.Filmes.Api.Profiles
+{
+    public class FilmeProfile : Profile
+    {
+        public FilmeProfile()
+        {
+            CreateMap<CreateFilmeDto, Filme>().ReverseMap();
+            CreateMap<ReadFilmeDto, Filme>().ReverseMap();
+            CreateMap<UpdateFilmeDto, Filme>().ReverseMap();
+        }
+    }
+}
---
+++ DotNet/WebAPI/Alura.Filmes.Api/Alura.Filmes.Api/Startup.cs
@@ -0,0 +1,57 @@
+using Alura.Filmes.Api.Data;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+using System;
+
+namespace Alura.Filmes.Api
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<FilmeContext>(opts =>
+                opts.UseSqlServer(Configuration.GetConnectionString("FilmeConnection")));
+            services.AddControllers();
+            services.AddAutoMapper((AppDomain.CurrentDomain.GetAssemblies()));
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Alura.Filmes.Api", Version = "v1" });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Alura.Filmes.Api v1"));
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
