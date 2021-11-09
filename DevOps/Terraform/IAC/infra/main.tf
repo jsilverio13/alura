9f4076f651e3e151f6d58bd3ae29985d11cbe621
jsilverio13 <jefferson.silverio@outlook.com>
2021-11-08 21:27:19 -0300

    Finalizando curso Infraestrutura como código com AWS e Terraform (#20)
    Co-authored-by: Jefferson Silverio <jsilverio@ciandt.com>

--- DevOps/Terraform/IAC/infra/main.tf
+++ DevOps/Terraform/IAC/infra/main.tf
@@ -16,15 +16,18 @@ provider "aws" {
 
 resource "aws_instance" "app_server" {
   ami           = "ami-03d5c68bab01f3496"
-  instance_type = "t2.micro"
-  key_name = "jsilverio-oregon"
-#   user_data = <<-EOF
-#     #!bin/bash
-#     cd /home/ubuntu
-#     echo "<h1>Feito com terraform</h1>" > index.html
-#     nohup busybox httpd -f -p 8080 &
-#                 EOF
+  instance_type = var.instancia
+  key_name = var.chave
   tags = {
     Name = "Terraform Ansible Python"
   }
+}
+
+resource "aws_key_pair" "chaveSSH" {
+    key_name = var.chave
+    public_key = file("${var.chave}.pub")
+}
+
+output "IP_publico" {
+    value = aws_instance.app_server.public_ip
 }
