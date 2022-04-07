b37529a07df83c94210e29f780c60cb8757ec106
Jefferson Silverio <jsilverio@ciandt.com>
2022-04-06 23:56:09 -0300

    Finalizando curso Angular: boas práticas em arquiteturas e formulários

---
+++ Angular/angular-boas-praticas-arquiteturas-formularios/gatitobook/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { RodapeModule } from './componentes/rodape/rodape.module';
+import { CabecalhoComponent } from './componentes/cabecalho/cabecalho.component';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { CabecalhoModule } from './componentes/cabecalho/cabecalho.module';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    CabecalhoModule,
+    RodapeModule
+
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
