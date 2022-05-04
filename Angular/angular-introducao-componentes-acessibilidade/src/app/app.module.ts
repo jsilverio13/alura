6f92d880f605573b89e7175e015cd50e318fc8b5
Jefferson Silverio <jsilverio@ciandt.com>
2022-05-03 23:26:39 -0300

    Primeiros passos com acessibilidade

--- Angular/angular-introducao-componentes-acessibilidade/src/app/app.module.ts
+++ Angular/angular-introducao-componentes-acessibilidade/src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { YesNoButtonGroupModule } from './shared/components/yes-no-button-group/yes-no-button-group.module';
+import { DisableControlModule } from './shared/directives/disable-control/disable-control.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    YesNoButtonGroupModule,
+    ReactiveFormsModule,
+    FormsModule,
+    DisableControlModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ Angular/angular-introducao-componentes-acessibilidade/src/app/shared/components/yes-no-button-group/yes-no-button-group.module.ts
@@ -0,0 +1,10 @@
+import { YesNoButtonGroupComponent } from './yes-no-button-group.component';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@NgModule({
+  declarations: [YesNoButtonGroupComponent],
+  imports: [CommonModule],
+  exports: [YesNoButtonGroupComponent],
+})
+export class YesNoButtonGroupModule {}
