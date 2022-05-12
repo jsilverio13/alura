944cfa32e0bd04a027d55f8657de37f18292d194
Jefferson Silverio <jsilverio@ciandt.com>
2022-05-12 17:49:29 -0300

    Finalizando curso Angular: testes automatizados com Jasmine e Karma

--- Angular/angular-jasmine-karma/karma.conf.js
+++ Angular/angular-jasmine-karma/karma.conf.js
@@ -8,9 +8,11 @@ module.exports = function (config) {
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
+      require('karma-firefox-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage'),
-      require('@angular-devkit/build-angular/plugins/karma')
+      require('@angular-devkit/build-angular/plugins/karma'),
+      require('karma-junit-reporter')
     ],
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
