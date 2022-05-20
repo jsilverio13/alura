bf478e08636e4b4fee00f33ade8ed44eeab620c0
Jefferson Silverio <jsilverio@ciandt.com>
2022-05-19 22:42:43 -0300

    Finalizando curso React: integrando seu projeto React com APIs

--- React/react-integrando-apis/alfood-main/src/App.tsx
+++ React/react-integrando-apis/alfood-main/src/App.tsx
@@ -1,4 +1,9 @@
 import { Routes, Route } from "react-router-dom";
+import PaginaBaseAdmin from "./pages/Administracao/PaginaBaseAdmin";
+import AdministracaoPratos from "./pages/Administracao/Pratos/AdministracaoPratos";
+import FormularioPrato from "./pages/Administracao/Pratos/FormularioPratos";
+import AdministracaoRestaurantes from "./pages/Administracao/Restaurantes/AdministracaoRestaurante";
+import FormularioRestaurante from "./pages/Administracao/Restaurantes/FormularioRestaurante";
 import Home from "./pages/Home";
 import VitrineRestaurantes from "./pages/VitrineRestaurantes";
 
@@ -7,6 +12,14 @@ function App() {
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/restaurantes" element={<VitrineRestaurantes />} />
+      <Route path="/admin/" element={<PaginaBaseAdmin />}>
+        <Route path="restaurantes" element={<AdministracaoRestaurantes />} />
+        <Route path="restaurantes/novo" element={<FormularioRestaurante />} />
+        <Route path="restaurantes/:id" element={<FormularioRestaurante />} />
+        <Route path="pratos" element={<AdministracaoPratos />} />
+        <Route path="pratos/novo" element={<FormularioPrato />} />
+        <Route path="pratos/:id" element={<FormularioPrato />} />
+      </Route>
     </Routes>
   );
 }
--- React/react-integrando-apis/alfood-main/src/pages/Home/index.tsx
+++ React/react-integrando-apis/alfood-main/src/pages/Home/index.tsx
@@ -11,7 +11,7 @@ function App() {
       <NavBar />
       <Banner />
       <div className={estilos.MiniBanners}>
-        <img src="/imagens/cozinhar_01.jpg" alt="Um prato conceitual" />
+        <img src="/images/cozinhar_01.jpg" alt="Um prato conceitual" />
         <div className={estilos.CardCentral}>
           <h2>A melhor rede de restaurantes!</h2>
           <div>
@@ -21,23 +21,23 @@ function App() {
             </p>
           </div>
         </div>
-        <img src="/imagens/cozinhar_02.jpg" alt="Um hambúrguer desconstruído" />
+        <img src="/images/cozinhar_02.jpg" alt="Um hambúrguer desconstruído" />
       </div>
       <div className={estilos.Categorias}>
         <div className={estilos.TipoDePrato}>
-          <img src="/imagens/cafedamanha.png" alt="Café da manhã" />
+          <img src="/images/cafedamanha.png" alt="Café da manhã" />
           <h4>Café da manhã</h4>
         </div>
         <div className={estilos.TipoDePrato}>
-          <img src="/imagens/almoco.png" alt="Almoço" />
+          <img src="/images/almoco.png" alt="Almoço" />
           <h4>Almoço</h4>
         </div>
         <div className={estilos.TipoDePrato}>
-          <img src="/imagens/jantar.png" alt="Jantar" />
+          <img src="/images/jantar.png" alt="Jantar" />
           <h4>Jantar</h4>
         </div>
         <div className={estilos.TipoDePrato}>
-          <img src="/imagens/sobremesa.png" alt="Sobremesas" />
+          <img src="/images/sobremesa.png" alt="Sobremesas" />
           <h4>Sobremesas</h4>
         </div>
       </div>
