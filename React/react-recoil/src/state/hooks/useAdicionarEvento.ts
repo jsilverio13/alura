c025f6642b75e335c8704341f559ebda1973fb66
Jefferson Silverio <jsilverio@ciandt.com>
2022-05-20 11:25:04 -0300

    Finalizando curso React: gerenciando estado com Recoil

---
+++ React/react-recoil/src/state/hooks/useAdicionarEvento.ts
@@ -0,0 +1,18 @@
+import { useSetRecoilState } from "recoil";
+import { IEvento } from "../../interfaces/IEvento";
+import { obterId } from "../../util";
+import { listaDeEventosState } from "../atom";
+
+const useAdicionarEvento = () => {
+  const setListaDeEventos = useSetRecoilState<IEvento[]>(listaDeEventosState);
+  return (evento: IEvento) => {
+    const hoje = new Date();
+    if (evento.inicio < hoje) {
+      throw new Error("Eventos não podem ser cadastrados antes de hoje!");
+    }
+    evento.id = obterId();
+    return setListaDeEventos((listaAntiga) => [...listaAntiga, evento]);
+  };
+};
+
+export default useAdicionarEvento;
