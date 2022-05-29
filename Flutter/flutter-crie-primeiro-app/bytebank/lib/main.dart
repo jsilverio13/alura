931c42e1aebf4e7d0d8cd964ff4bd3402dea224b
Jefferson Silverio <jsilverio@ciandt.com>
2022-05-29 18:58:37 -0300

    Flutter: criando um app

---
+++ Flutter/flutter-crie-primeiro-app/bytebank/lib/main.dart
@@ -0,0 +1,24 @@
+// ignore_for_file: use_key_in_widget_constructors, prefer_const_constructors, prefer_const_constructors_in_immutables
+
+import 'package:bytebank/screens/transferencia/lista.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(BytebankApp());
+
+class BytebankApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primaryColor: Colors.green[900],
+        buttonTheme: ButtonThemeData(
+          buttonColor: Colors.blueAccent[700],
+          textTheme: ButtonTextTheme.primary,
+        ),
+        colorScheme: ColorScheme.fromSwatch()
+            .copyWith(secondary: Colors.blueAccent[700]),
+      ),
+      home: ListaTransferencias(),
+    );
+  }
+}
---
+++ Flutter/flutter-crie-primeiro-app/bytebank/lib/screens/transferencia/lista.dart
@@ -0,0 +1,68 @@
+// ignore_for_file: use_key_in_widget_constructors
+
+import 'package:bytebank/models/transferencia.dart';
+import 'package:bytebank/screens/transferencia/formulario.dart';
+import 'package:flutter/material.dart';
+
+const _tituloAppBar = 'Transferências';
+
+class ListaTransferencias extends StatefulWidget {
+  final List<Transferencia> _transferencias = [];
+
+  ListaTransferencias({Key? key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() {
+    return ListaTransferenciasState();
+  }
+}
+
+class ListaTransferenciasState extends State<ListaTransferencias> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(_tituloAppBar),
+      ),
+      body: ListView.builder(
+        itemCount: widget._transferencias.length,
+        itemBuilder: (context, indice) {
+          final transferencia = widget._transferencias[indice];
+          return ItemTransferencia(transferencia);
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add),
+        onPressed: () {
+          Navigator.push(context, MaterialPageRoute(builder: (context) {
+            return const FormularioTransferencia();
+          })).then(
+            (transferenciaRecebida) => _atualiza(transferenciaRecebida),
+          );
+        },
+      ),
+    );
+  }
+
+  void _atualiza(Transferencia transferenciaRecebida) {
+    setState(() {
+      widget._transferencias.add(transferenciaRecebida);
+    });
+  }
+}
+
+class ItemTransferencia extends StatelessWidget {
+  final Transferencia _transferencia;
+
+  const ItemTransferencia(this._transferencia);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+        child: ListTile(
+      leading: const Icon(Icons.monetization_on),
+      title: Text(_transferencia.valor.toString()),
+      subtitle: Text(_transferencia.numeroConta.toString()),
+    ));
+  }
+}
