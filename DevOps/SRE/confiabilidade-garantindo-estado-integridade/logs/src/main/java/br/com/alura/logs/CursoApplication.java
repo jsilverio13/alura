4144aaa454bcbcd17c3fd5c156ef71707f7108e6
Jefferson Silverio <jsilverio@ciandt.com>
2022-10-17 23:15:35 -0300

    Primeira aula do curso Confiabilidade: garantindo o estado de integridade de sistemas através de logs

---
+++ DevOps/SRE/confiabilidade-garantindo-estado-integridade/logs/src/main/java/br/com/alura/logs/CursoApplication.java
@@ -0,0 +1,19 @@
+package br.com.alura.logs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+@SpringBootApplication
+public class CursoApplication {
+	
+	private static Logger logger = LoggerFactory.getLogger(CursoApplication.class);
+
+	public static void main(String[] args) {
+		logger.info("Iniciando a API de Cursos da Alura");
+		SpringApplication.run(CursoApplication.class, args);
+		logger.info("API para cadastro para cursos da plataforma Alura.");
+	}
+}
