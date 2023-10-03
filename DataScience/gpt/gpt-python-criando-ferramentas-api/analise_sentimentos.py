d0a8b2dca3ddce383d448425220a4af8980df08e
Jefferson Silverio <jsilverio@ciandt.com>
2023-10-02 21:12:56 -0300

    Finalizando curso GPT e Python: criando ferramentas com a API

--- DataScience/gpt/gpt-python-criando-ferramentas-api/analise_sentimentos.py
+++ DataScience/gpt/gpt-python-criando-ferramentas-api/analise_sentimentos.py
@@ -4,11 +4,6 @@ import openai
 import dotenv
 import tiktoken
 
-dotenv.load_dotenv()
-openai.api_key = ""
-os.system('clear')
-
-
 def analise_sentimento(nome_do_produto):
     prompt_sistema = """
     Você é um analisador de sentimentos de avaliações de produtos.
@@ -27,32 +22,40 @@ def analise_sentimento(nome_do_produto):
 
     print(f"Iniciando a análise do produto: {nome_do_produto}")
     tentativas = 0
+    tempo_de_espera = 2
     while tentativas < 3:
         try:
+
             tentativas += 1
             print(f"Tentativa numero {tentativas}")
-            raise openai.error.APIError
-            # resposta = openai.ChatCompletion.create(
-            #     model="gpt-3.5-turbo",
-            #     messages=[
-            #         {
-            #             "role": "system",
-            #             "content": prompt_sistema
-            #         },
-            #         {
-            #             "role": "user",
-            #             "content": prompt_usuario
-            #         }
-            #     ]
-            # )
+        
+            resposta = openai.ChatCompletion.create(
+                model="gpt-3.5-turbo",
+                messages=[
+                    {
+                        "role": "system",
+                        "content": prompt_sistema
+                    },
+                    {
+                        "role": "user",
+                        "content": prompt_usuario
+                    }
+                ]
+            )
 
             salva(f"./dados/analise-{nome_do_produto}", resposta.choices[0].message.content)
             print("Análise concluída com sucesso!")
+            return 
+
         except openai.error.AuthenticationError as e:
             print(f"Erro de autenticacao: {e}")
         except openai.error.APIError as e:
             print(f"Erro de api: {e}")
-            time.sleep(2)                      
+            time.sleep(2)      
+        except openai.error.RateLimitError as e:
+            print(f"Erro de limite de taxa: {e}")
+            time.sleep(tempo_de_espera)
+            tempo_de_espera *= 2
         
 
 def carrega(nome_do_arquivo):
@@ -71,7 +74,11 @@ def salva(nome_do_arquivo, conteudo):
             print(f"Erro ao salvar arquivo: {e}")
 
 
-lista_de_produtos = ["Tapete de yoga", "Tabuleiro de xadrez de madeira"]
+dotenv.load_dotenv()
+openai.api_key = os.getenv("OPENAI_API_KEY")
+os.system('clear')
+
+lista_de_produtos = ["DVD player automotivo", "Esteira elétrica para fitness", "Grill elétrico para churrasco", "Mixer de sucos e vitaminas", "Tapete de yoga", "Miniatura de carro colecionável", "Balança de cozinha digital", "Jogo de copos e taças de cristal", "Tabuleiro de xadrez de madeira", "Boia inflável para piscina"]
 
 for nome_do_produto in lista_de_produtos:
-    analise_sentimento(nome_do_produto)
+    analise_sentimento (nome_do_produto)
