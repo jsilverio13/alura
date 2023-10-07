b400e5ebe831926a2649ef21e0dfe1d386c60135
jsilverio13 <jefferson.silverio@outlook.com>
2023-10-07 15:17:49 -0300

    Finalizando curso OpenAI: criando uma postagem para o Instagram com IA Generativa (#50)
    Co-authored-by: Jefferson Silverio <jsilverio@ciandt.com>

---
+++ DataScience/gpt/openai-criando-postagem-instagram-ia-generativa/main.py
@@ -0,0 +1,290 @@
+import openai
+from dotenv import load_dotenv
+import os
+import requests
+from pydub import AudioSegment
+from PIL import Image 
+from instabot import Bot
+import shutil
+
+def openai_whisper_transcrever(caminho_audio, nome_arquivo, modelo_whisper, openai):
+    print("Estou transcrevendo com o whispers ...")
+
+    audio = open(caminho_audio, "rb")
+
+    resposta = openai.Audio.transcribe(
+        api_key=openai.api_key,
+        model = modelo_whisper,
+        file = audio
+    )
+
+    transcricao = resposta.text
+
+    with open(f"texto_completo_{nome_arquivo}.txt", "w",encoding='utf-8') as arquivo_texto:
+        arquivo_texto.write(transcricao)
+
+    return transcricao
+
+def openai_gpt_resumir_texto(transcricao_completa, nome_arquivo, openai):
+    print("Resumindo com o gpt para um post do instagram ...")
+
+    prompt_sistema = """
+    Assuma que você é um digital influencer digital e que está construíndo conteúdos das áreas de tecnologia em uma plataforma de áudio (podcast).
+
+    Os textos produzidos devem levar em consideração uma peresona que consumirá os conteúdos gerados. Leve em consideração:
+
+    - Seus seguidores são pessoas super conectadas da área de tecnologia, que amam consumir conteúdos relacionados aos principais temas da área de computação.
+    - Você deve utilizar o gênero neutro na construção do seu texto
+    - Os textos serão utilizados para convidar pessoas do instagram para consumirem seu conteúdo de áudio
+    - O texto deve ser escrito em português do Brasil.
+
+    """
+    prompt_usuario = ". \nReescreva a transcrição acima para que possa ser postado como uma legenda do Instagram. Ela deve resumir o texto para chamada na rede social. Inclua hashtags"
+
+    resposta = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo-16k",
+        messages=[
+            {
+                "role": "system",
+                "content" : prompt_sistema
+            },
+            {
+                "role": "user",
+                "content": transcricao_completa + prompt_usuario
+            }
+        ],
+        temperature = 0.6
+    )
+
+    resumo_instagram = resposta["choices"][0]["message"]["content"]
+
+    with open(f"resumo_instagram_{nome_arquivo}.txt", "w",encoding='utf-8') as arquivo_texto:
+        arquivo_texto.write(resumo_instagram)
+
+    return resumo_instagram
+
+def ferramenta_ler_arquivo(nome_arquivo):
+    try:
+        with open(nome_arquivo, "rb") as arquivo:
+            return arquivo.read()
+    except IOError as e:
+        print(f"Erro no carregamento de arquivo: {e}")
+
+def openai_gpt_criar_hashtag(resumo_instagram, nome_arquivo, openai):
+    print("Gerando as hashtags com a open ai ... ")
+
+    prompt_sistema = """
+    Assuma que você é um digital influencer digital e que está construíndo conteúdos das áreas de tecnologia em uma plataforma de áudio (podcast).
+
+    Os textos produzidos devem levar em consideração uma peresona que consumirá os conteúdos gerados. Leve em consideração:
+
+    - Seus seguidores são pessoas super conectadas da área de tecnologia, que amam consumir conteúdos relacionados aos principais temas da área de computação.
+    - Você deve utilizar o gênero neutro na construção do seu texto
+    - Os textos serão utilizados para convidar pessoas do instagram para consumirem seu conteúdo de áudio
+    - O texto deve ser escrito em português do Brasil.
+    - A saída deve conter 5 hashtags.
+
+    """
+
+    prompt_usuario =f'Aqui está um resumo de um texto "{resumo_instagram}". Por favor, gere 5 hashtags que sejam relevantes para este texto e que possam ser publicadas no Instagram.  Por favor, faça isso em português do Brasil '
+
+    resposta = openai.ChatCompletion.create(
+        model="gpt-4",
+        messages=[
+            {
+                "role": "system",
+                "content" : prompt_sistema
+            },
+            {
+                "role": "user",
+                "content": prompt_usuario
+            }
+        ],
+        temperature = 0.6
+    )
+
+    hashtags = resposta["choices"][0]["message"]["content"]
+
+    with open(f"hashtag_{nome_arquivo}.txt", "w",encoding='utf-8') as arquivo_texto:
+        arquivo_texto.write(hashtags)
+    
+    return hashtags
+
+def openai_gpt_gerar_texto_imagem(resumo_instagram, nome_arquivo, openai):
+    print("Gerando a saida de texto para criacao de imagens com o GPT ...")
+
+    prompt_sistema = """
+
+    - A saída deve ser uma única, do tamanho de um tweet, que seja capaz de descrever o conteúdo do texto para que possa ser transcrito como uma imagem.
+    - Não inclua hashtags
+
+    """
+
+    prompt_usuario =  f'Reescreva o texto a seguir, em uma frase, para que descrever o texto abaixo em um tweet: {resumo_instagram}'
+    
+    resposta = openai.ChatCompletion.create(
+        model="gpt-4",
+        messages=[
+            {
+                "role": "system",
+                "content" : prompt_sistema
+            },
+            {
+                "role": "user",
+                "content": prompt_usuario
+            }
+        ],
+        temperature = 0.6
+    )
+
+    texto_para_imagem = resposta["choices"][0]["message"]["content"]
+
+    with open(f"texto_para_geracao_imagem_{nome_arquivo}.txt", "w",encoding='utf-8') as arquivo_texto:
+        arquivo_texto.write(texto_para_imagem)
+    
+    return texto_para_imagem
+
+def openai_dalle_gerar_imagem(resolucao, resumo_para_imagem, nome_arquivo, openai, qtd_imagens = 1):
+    print("Criando uma imagem utilizando a API do DALL-E ...")
+
+    prompt_user = f"Uma pintura ultra futurista, textless, 3d que retrate: {resumo_para_imagem}"
+    
+    resposta = openai.Image.create(
+        prompt =prompt_user,
+        n = qtd_imagens,
+        size = resolucao
+    )
+
+    print(resposta["data"][0].url)
+
+    return resposta["data"]
+
+def ferramenta_download_imagem(nome_arquivo, imagem_gerada,qtd_imagens = 1):
+  lista_nome_imagens = []
+  try:
+    for contador_imagens in range(0,qtd_imagens):
+        camimho = imagem_gerada[contador_imagens].url
+        imagem = requests.get(camimho)
+
+        with open(f"{nome_arquivo}_{contador_imagens}.png", "wb") as arquivo_imagem:
+            arquivo_imagem.write(imagem.content)
+
+        lista_nome_imagens.append(f"{nome_arquivo}_{contador_imagens}.png")
+    return lista_nome_imagens
+  except:
+    print("Ocorreu um erro!")
+    return  None
+
+def ferramenta_transcrever_audio_em_partes(caminho_audio_podcast, nome_arquivo):
+    print("Iniciando corte .. ")
+    audio = AudioSegment.from_mp3(caminho_audio_podcast)
+
+    dez_minutos = 10 * 60 * 1000
+    
+    contador_pedaco = 1
+    arquivos_exportados = []
+
+    while len(audio) > 0:
+        pedaco = audio[:dez_minutos]
+        nome_pedaco_audio = f"{nome_arquivo}_parte_{contador_pedaco}.mp3"
+        pedaco.export(nome_pedaco_audio, format="mp3")
+        arquivos_exportados.append(nome_pedaco_audio)
+        audio = audio[dez_minutos:]
+        contador_pedaco += 1
+
+    return arquivos_exportados
+
+def openai_whisper_transcrever_em_partes(caminho_audio, nome_arquivo, modelo_whisper, openai):
+    print("Estou transcrevendo com o whispers ...")
+
+    lista_arquivos_de_audio = ferramenta_transcrever_audio_em_partes(caminho_audio, nome_arquivo)
+    lista_pedacos_de_audio = []
+
+    for um_pedaco_audio in lista_arquivos_de_audio:
+        audio = open(um_pedaco_audio, "rb")
+
+        resposta = openai.Audio.transcribe(
+            api_key=openai.api_key,
+            model = modelo_whisper,
+            file = audio
+        )
+
+        transcricao = resposta.text
+        lista_pedacos_de_audio.append(transcricao)
+    
+    transcricao = "".join(lista_pedacos_de_audio)
+
+    with open(f"texto_completo_{nome_arquivo}.txt", "w",encoding='utf-8') as arquivo_texto:
+        arquivo_texto.write(transcricao)
+
+    return transcricao
+
+def selecionar_imagem(lista_nome_imagens):
+    opcao = int(input("Qual imagem você deseja selecionar, informe o numero do sufixo da imagem gerada? "))
+    if opcao < 0 or opcao > 3:
+        opcao = 0
+    return lista_nome_imagens[opcao]
+
+
+def ferramenta_converte_string(dado):
+    if isinstance(dado, bytes):
+        return str(dado.decode())
+    return dado
+
+def ferramenta_converter_png_para_jpg(caminho_imagem_escolhida, nome_arquivo):
+    img_png = Image.open(caminho_imagem_escolhida)
+    img_png.save(caminho_imagem_escolhida.split(".")[0]+".jpg")
+
+    return caminho_imagem_escolhida.split(".")[0] + ".jpg"
+
+def postar_instagram(caminho_imagem, texto, user, password):
+    if os.path.exists("config"):
+        shutil.rmtree("config")
+    bot = Bot()
+    
+    bot.login(username=user, password=password)
+
+    resposta = bot.upload_photo(caminho_imagem, caption=texto)
+
+def confirmar_postagem(caminho_imagem_convertida, legenda_postagem):
+    print(f"\nCaminho Imagem: {caminho_imagem_convertida}")
+    print(f"\Legenda: {legenda_postagem}")
+
+    print("\n\nDeseja postar os dados acima no seu instagram? Digite 's' para sim e 'n' para não.")
+    return input()
+
+def main():
+    load_dotenv()
+
+    caminho_audio = "podcasts/hipsters_154_testes_short.mp3"
+    nome_arquivo = "hipsters_154_testes_short"
+    url_podcast = "https://www.hipsters.tech/testes-de-software-e-inteligencia-artificial-hipsters-154/"
+    
+    api_openai = os.getenv("API_KEY_OPENAI")
+    openai.api_key = api_openai
+    
+    resolucao = "1024x1024"
+    qtd_imagens = 1
+    usuario_instagram = os.getenv("USER_INSTAGRAM")
+    senha_instagram = os.getenv("PASSWORD_INSTAGRAM")
+
+    modelo_whisper = "whisper-1"
+
+    transcricao_completa = openai_whisper_transcrever(caminho_audio,nome_arquivo,modelo_whisper,openai)
+    resumo_instagram = openai_gpt_resumir_texto(transcricao_completa, nome_arquivo, openai)
+    hashtags = openai_gpt_criar_hashtag(resumo_instagram, nome_arquivo, openai)
+    resumo_imagem_instagram = openai_gpt_gerar_texto_imagem(resumo_instagram, nome_arquivo, openai)
+    imagem_gerada = openai_dalle_gerar_imagem(resolucao, resumo_imagem_instagram, nome_arquivo, openai, qtd_imagens )
+    lista_nome_imagens = ferramenta_download_imagem(nome_arquivo,imagem_gerada, qtd_imagens)
+    
+    caminho_imagem_escolhida = selecionar_imagem(lista_nome_imagens)
+    caminho_imagem_convertida = ferramenta_converter_png_para_jpg(caminho_imagem_escolhida, nome_arquivo)
+    
+    if confirmar_postagem(caminho_imagem_convertida, "Link do Podcast: " + url_podcast + " \n" + ferramenta_converte_string(resumo_instagram) + "\n" + ferramenta_converte_string(hashtags)).lower() == "s":
+            postar_instagram(caminho_imagem_convertida, 
+                        "Link do Podcast: " + url_podcast + " \n" + ferramenta_converte_string(resumo_instagram) + "\n" + ferramenta_converte_string(hashtags), 
+                        usuario_instagram, 
+                        senha_instagram)
+
+if __name__ == "__main__":
+    main()  
