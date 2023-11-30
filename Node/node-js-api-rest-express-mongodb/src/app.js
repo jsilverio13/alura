4a820f87a366c1e218cfb7b6c75dc897c555774d
Jefferson Silverio <jsilverio@ciandt.com>
2023-11-29 21:55:40 -0300

    Criação de routes e nova controller de autores

--- Node/node-js-api-rest-express-mongodb/src/app.js
+++ Node/node-js-api-rest-express-mongodb/src/app.js
@@ -1,56 +1,18 @@
 import express from "express";
+import conectaDatabase from "./config/dbConnect.js";
+import routes from "./routes/index.js";
+
+const conexao = await conectaDatabase();
+
+conexao.on("error", (erro) => {
+  console.error("Erro ao conectar no banco", erro);
+});
+
+conexao.once("open", () => {
+  console.log("Conectado com sucesso!");
+});
 
 const app = express();
-app.use(express.json());
-
-const livros = [
-    {
-        id: 1,
-        titulo: "O senhor dos anéis"
-    },
-    {
-        id: 2,
-        titulo: "O Hobbit"
-    }
-]
-
-function buscaLivro(id) {
-    return livros.findIndex(livro => {
-        return livro.id === id
-    })
-}
-
-app.get("/", (req, res) => {
-    res.status(200).send("Curso de node.js");
-})
-
-app.get("/livros", (req, res) => {
-    res.status(200).json(livros);
-})
-
-app.get("/livros/:id", (req, res) => {
-    let id = Number(req.params.id);
-    const index = buscaLivro(id);
-    res.status(200).json(livros[index]);
-})
-
-app.post("/livros", (req, res) => {
-    livros.push(req.body);
-    res.status(201).send("Livro cadastrado com sucesso");
-})
-
-app.put("/livros/:id", (req, res) => {
-    let id = Number(req.params.id);
-    const index = buscaLivro(id);
-    livros[index].titulo = req.body.titulo;
-    res.status(201).send("Livro atualizado com sucesso");
-})
-
-app.delete("/livros/:id", (req, res) => {
-    let id = Number(req.params.id);
-    const index = buscaLivro(id);
-    livros.splice(index);
-    res.status(200).send("Livro removido com sucesso");
-})
+routes(app);
 
 export default app;
---
+++ Node/node-js-api-rest-express-mongodb/src/controllers/livroController.js
@@ -0,0 +1,53 @@
+import livro from "../models/Livro.js";
+
+class LivroController {
+  static async listarLivros(req, res) {
+    try {
+      const listaLivros = await livro.find({});
+      res.status(200).json(listaLivros);
+    } catch (erro) {
+      res.status(500).json({ message: `${erro.message} - falha na requisição` });
+    }
+  }
+
+  static async cadastrarLivros(req, res) {
+    try {
+      const novoLivro = await livro.create(req.body);
+      res.status(201).json({ message: "Criado com sucesso", livro: novoLivro });
+    } catch (erro) {
+      res.status(500).json({ message: `${erro.message} - falha na requisição` });
+    }
+  }
+
+  static async listarLivroPorId(req, res) {
+    try {
+      const id = req.params.id;
+      const livroEncontrado = await livro.findById(id);
+      res.status(200).json({ message: "Livro encontrado", livro: livroEncontrado });
+    } catch (erro) {
+      res.status(500).json({ message: `${erro.message} - falha na requisição` });
+    }
+  }
+
+  static async atualizarLivro(req, res) {
+    try {
+      const id = req.params.id;
+      await livro.findByIdAndUpdate(id, req.body);
+      res.status(200).json({ message: "Livro atualizado" });
+    } catch (erro) {
+      res.status(500).json({ message: `${erro.message} - falha na requisição` });
+    }
+  }
+
+  static async excluirLivro(req, res) {
+    try {
+      const id = req.params.id;
+      await livro.findByIdAndDelete(id);
+      res.status(204).json({ message: "Livro removido" });
+    } catch (erro) {
+      res.status(500).json({ message: `${erro.message} - falha na requisição` });
+    }
+  }
+}
+
+export default LivroController;
