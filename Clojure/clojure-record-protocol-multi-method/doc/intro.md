05543ccf2512f68e3a7a363e63b29ec5e86be574
jsilverio <jefferson.silverio@outlook.com>
2024-04-23 18:08:17 -0300

    Inicio do curso Clojure: Record, protocol e multi method

---
+++ Clojure/clojure-record-protocol-multi-method/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to clojure-record-protocol-multi-method
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ Clojure/clojure-record-protocol-multi-method/test/clojure_record_protocol_multi_method/core_test.clj
@@ -0,0 +1,7 @@
+(ns clojure-record-protocol-multi-method.core-test
+  (:require [clojure.test :refer :all]
+            [clojure-record-protocol-multi-method.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
