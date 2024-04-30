e8707f332a954959b7e2c5efaf1d731ab2921986
root <root@personal>
2024-04-29 23:20:44 -0300

    Inicio do curso Datomic Queries: avançando com o modelo e pesquisas

---
+++ Datomic/datomic-identidade-e-queries/src/datomic_identidade_e_queries/core.clj
@@ -0,0 +1,5 @@
+(ns datomic-identidade-e-queries.core
+  (:require [clojure.pprint :refer [pprint]]))
+
+
+(pprint "It Works")
---
+++ Datomic/datomic-identidade-e-queries/test/datomic_identidade_e_queries/core_test.clj
@@ -0,0 +1,7 @@
+(ns datomic-identidade-e-queries.core-test
+  (:require [clojure.test :refer :all]
+            [datomic-identidade-e-queries.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
