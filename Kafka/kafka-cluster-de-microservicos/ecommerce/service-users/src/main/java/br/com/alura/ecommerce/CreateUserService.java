556298022e8c520c29023a6b55839e23cf7abcf4
jsilverio <jefferson.silverio@outlook.com>
2024-05-04 12:35:41 -0300

    Finalizando aula 4 sobre Kafka: Fast delegate, evolução e cluster de brokers

---
+++ Kafka/kafka-cluster-de-microservicos/ecommerce/service-users/src/main/java/br/com/alura/ecommerce/CreateUserService.java
@@ -0,0 +1,69 @@
+package br.com.alura.ecommerce;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Map;
+import java.util.UUID;
+import java.util.concurrent.ExecutionException;
+
+@SuppressWarnings("CallToPrintStackTrace")
+public class CreateUserService {
+    private final KafkaDispatcher<Order> orderDispatcher = new KafkaDispatcher<>();
+    private final Connection connection;
+
+    CreateUserService() throws SQLException {
+        String url = "jdbc:sqlite:target/users_database.db";
+        this.connection = DriverManager.getConnection(url);
+        try {
+            connection.createStatement().execute("create table Users (" +
+                    "uuid varchar(200) primary key," +
+                    "email varchar(200))");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) throws ExecutionException, InterruptedException, SQLException {
+        var createUserService = new CreateUserService();
+        try (var service = new KafkaService<>(CreateUserService.class.getSimpleName(),
+                "ECOMMERCE_NEW_ORDER",
+                createUserService::parse,
+                Order.class,
+                Map.of())) {
+            service.run();
+        }
+    }
+
+    private void parse(ConsumerRecord<String, Order> record) throws ExecutionException, InterruptedException, SQLException {
+        System.out.println("------------------------------------------");
+        System.out.println("Processing new order, checking for new user");
+        System.out.println(record.value());
+        var order = record.value();
+        System.out.println(order.getEmail());
+        if (isNewUser(order.getEmail())) {
+            insertNewUser(order.getEmail());
+        }
+        System.out.println("Order processed");
+    }
+
+    private void insertNewUser(String email) throws SQLException {
+        var insert = connection.prepareStatement("insert into Users (uuid, email) " +
+                "values (?,?)");
+        insert.setString(1, UUID.randomUUID().toString());
+        insert.setString(2, email);
+        insert.execute();
+        System.out.println("Usuário uuid e " + email + " adicionado");
+    }
+
+    private boolean isNewUser(String email) throws SQLException {
+        var exists = connection.prepareStatement("select uuid from Users " +
+                "where email = ? limit 1");
+        exists.setString(1, email);
+        var results = exists.executeQuery();
+        return !results.next();
+    }
+
+}
