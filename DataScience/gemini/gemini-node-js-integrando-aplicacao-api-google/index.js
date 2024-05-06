f89883461dda0e64f9934c65ecb7f01ffc6bcd9a
jsilverio <jefferson.silverio@outlook.com>
2024-05-05 23:05:56 -0300

    Finalizando curso Gemini e Node.js: integrando sua aplicação com a API do Google

--- DataScience/gemini/gemini-node-js-integrando-aplicacao-api-google/index.js
+++ DataScience/gemini/gemini-node-js-integrando-aplicacao-api-google/index.js
@@ -1,23 +1,29 @@
-import { GoogleGenerativeAI } from "@google/generative-ai";
-import { fazerPergunta } from "./pergunta.js";
+import { fazerPergunta } from "./service/pergunta.js";
+import { perguntar } from "./service/perguntaLivre.js";
+import { consultar } from "./service/consultaDestino.js";
+import { processaImagem } from "./service/processaImagem.js";
+import { processaArquivoTexto } from "./service/categorizador.js";
 
-// Access your API key as an environment variable (see "Set up your API key" above)
-const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
+async function principal() {
+  const escolha = await fazerPergunta(`Escolha uma das opções abaixo: \n
+  1. Fazer uma pergunta livre sobre um destino;
+  2. Comparação de destinos por categorias;
+  3. Ver informações com base em uma imagem;
+  4. Fazer a análise de sentimentos baseado em arquivo texto
+  \nOpção desejada: `);
 
-async function run() {
-  // For text-only input, use the gemini-pro model
-  const model = genAI.getGenerativeModel({ model: "gemini-pro" });
-
-  let prompt =
-    "Você é um site de viagens e deve responder somente sobre esse assunto" +
-    " Caso o usuário pergunte sobre algo diferente, diga que não pode responder. " +
-    " O usuário escolheu: ";
-  prompt += await fazerPergunta("Me fale sobre o destino que deseja conhecer: ");
-
-  const result = await model.generateContent(prompt);
-  const response = result.response;
-  const text = response.text();
-  console.log(text);
+  if (escolha === "1") {
+    await perguntar();
+  } else if (escolha === "2") {
+    await consultar();
+  } else if (escolha === "3") {
+    const imagem = await fazerPergunta("\nMe informe o caminho completo e nome da imagem: ");
+    await processaImagem(imagem);
+  } else if (escolha === "4") {
+    await processaArquivoTexto();
+  } else {
+    console.log("Escolha inválida.");
+  }
 }
 
-run();
+principal();
