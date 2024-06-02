9fe7ad746066d41cc6e56f760ec66de33fc77100
Jefferson Silverio <jefferson.silverio@nubank.com.br>
2024-06-02 00:24:39 -0300

    Finishing lesson 01 of course python-aplicando-orientacao-objetos

---
+++ Python/formacao-ooo/python-aplicando-orientacao-objetos/sabor-express/modelos/restaurante.py
@@ -0,0 +1,16 @@
+class Restaurante:
+    nome = ""
+    categoria = ""
+    ativo = False
+
+
+restaurante_praca = Restaurante()
+restaurante_praca.nome = "Praça"
+restaurante_praca.categoria = "Gourmet"
+restaurante_pizza = Restaurante()
+restaurante_pizza = "Pizza"
+restaurante_pizza = "Italiana"
+
+restaurantes = [restaurante_pizza, restaurante_praca]
+
+print(restaurantes)
