bcc8d05d6a29d4768e1017449412354eed4fa583
Jefferson Silverio <jefferson.silverio@nubank.com.br>
2024-07-24 19:45:00 -0300

    Begin course Flutter: aplicando testes de unidade, de Widget e Mocks

---
+++ Flutter/formacao-flutter/flutter-testes-unidade-widget-mocks/lib/data/bank_inherited.dart
@@ -0,0 +1,26 @@
+import 'package:estilizacao_componentes/models/bank.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+class BankInherited extends InheritedWidget {
+  BankInherited({
+    Key? key,
+    required Widget child,
+  }) : super(key: key, child: child);
+
+  final BankModel values = BankModel();
+
+  static BankInherited of(BuildContext context) {
+    final BankInherited? result = context.dependOnInheritedWidgetOfExactType<BankInherited>();
+    assert(result != null, 'No BankInherited found in context');
+    return result!;
+  }
+
+  @override
+  bool updateShouldNotify(BankInherited oldWidget) {
+    if (kDebugMode) {
+      print('updateShouldNotify: ${oldWidget.values.points}');
+    }
+    return values.points != oldWidget.values.points;
+  }
+}
